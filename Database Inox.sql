3f7e5460089fe2c64e8ed953683813844083444c
thuantv9 <tvthuan.sldtbxhtb@gmail.com>
2018-09-14 10:06:12 +0700

    Thêm split string trong sql để tách trường ảnh

--- Database Inox.sql
+++ Database Inox.sql
@@ -574,3 +574,31 @@ begin
 end
 go
 
+-- Thử nghiệm split string trong SQL
+CREATE FUNCTION dbo.splitstring ( @stringToSplit VARCHAR(MAX) )
+RETURNS
+ @returnList TABLE ([Name] [nvarchar] (500))
+AS
+BEGIN
+
+ DECLARE @name NVARCHAR(255)
+ DECLARE @pos INT
+
+ WHILE CHARINDEX(',', @stringToSplit) > 0
+ BEGIN
+  SELECT @pos  = CHARINDEX(',', @stringToSplit)  
+  SELECT @name = SUBSTRING(@stringToSplit, 1, @pos-1)
+
+  INSERT INTO @returnList 
+  SELECT @name
+
+  SELECT @stringToSplit = SUBSTRING(@stringToSplit, @pos+1, LEN(@stringToSplit)-@pos)
+ END
+
+ INSERT INTO @returnList
+ SELECT @stringToSplit
+
+ RETURN
+END
+
+
